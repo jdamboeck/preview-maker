6080ab0833a2f3d32ea260d690fc0f4ecba85e14
ajdamboeck <j.damboeck@googlemail.com>
2025-03-17 11:31:26 +0100

    chore: Add PyRight config and type stubs for GTK

---
+++ typings/gi/__init__.pyi
@@ -0,0 +1,10 @@
+from typing import Any
+
+def require_version(namespace: str, version: str) -> None:
+    """Set a requirement for namespace to be introspected.
+
+    Args:
+        namespace: GI namespace to require (e.g. 'Gtk')
+        version: Version to require (e.g. '4.0')
+    """
+    pass
---
+++ typings/gi/repository/__init__.pyi
@@ -0,0 +1,33 @@
+from typing import Any
+
+class Gtk:
+    Window: Any
+    Box: Any
+    Button: Any
+    Label: Any
+    Image: Any
+    # Add other Gtk classes as needed
+
+class Gdk:
+    RGBA: Any
+    Display: Any
+    # Add other Gdk classes as needed
+
+class GLib:
+    idle_add: Any
+    timeout_add: Any
+    MainLoop: Any
+    # Add other GLib classes as needed
+
+class Gio:
+    File: Any
+    # Add other Gio classes as needed
+
+class GdkPixbuf:
+    Pixbuf: Any
+    # Add other GdkPixbuf classes as needed
+
+class Notify:
+    init: Any
+    Notification: Any
+    # Add other Notify classes as needed
